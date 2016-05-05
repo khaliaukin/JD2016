c47449318a6325b24fc73f0a77a942490ebb416c
VasilevichSergey <03022007sv>
2016-05-05 12:07:53 +0300

    jd01_02 jd01_04

--- src/by/it/vasilevich/jd01_04/InOut.java
+++ src/by/it/vasilevich/jd01_04/InOut.java
@@ -1,62 +1,52 @@
 package by.it.vasilevich.jd01_04;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class InOut {
-    static String getOneLine() throws IOException {
-        BufferedReader stin = new BufferedReader(new InputStreamReader(System.in));
-        String line = stin.readLine();
-        stin.close();
-        return line;
-    }
-
-
-    public static double[] lineToIntArray(String line) {
-        String strMas[] = line.split(" ");
-        // разбили строку line разделителем " ";
-        // получили массив strMas{введенные числа};
-        double dMas[] = new double[strMas.length];
-        // в массив intMas[] вносятся целые числа массива strMas[];
-        for (int i = 0; i < strMas.length; i++) {
-            dMas[i] = Double.parseDouble(strMas[i]);
-        }
-        return dMas;
-    }
-
-    public static void arrayOne(String[] arg) throws IOException {
+    public static void main(String[] args) throws IOException {
         System.out.println("Введите числа для одномерного массива:");
-        String line = getOneLine();
+        Scanner in = new Scanner(System.in);
+        String line2 = in.nextLine();
         //строка ввода из консоли;
-        double arr[] = lineToIntArray(line);
+        double array[] = Utils.lineToIntArray(line2);
         //преобразуем строку в массив;
-        //System.out.println(arr[5]);
         System.out.print("Создан одномерный массив со следующими значениями:\n{");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
             //выводим все элементы массива;
-            if (i < arr.length - 1)
+            if (i < array.length - 1)
                 System.out.print(", ");
             else
                 System.out.print("}");
         }
+
+        System.out.println("\nВведите размер двумерной матрицы типа N*N:");
+        Scanner n = new Scanner(System.in);
+        int size = n.nextInt();
+        //устанавливаем размер матрицы;
+        double array2[][] = new double[size][size];
+        System.out.print("Введите двумерный массив:\n");
+        for (int i = 0; i < array2.length; i++)
+            for (int j = 0; j < array2.length; j++) {
+                System.out.println("Введите [" + i + "][" + j + "]-ый компонент массива");
+                array2[i][j] = n.nextInt();
+            }
+        System.out.print("Создана двумерная матрица со следующими значениями:\n");
+        for (double[] anArr2 : array2) {
+            for (int j = 0; j < array2.length; j++) {
+                System.out.print(anArr2[j]);
+                //выводим все элементы массива;
+                if (j < array2.length - 1)
+                    System.out.print(", ");
+                else
+                    System.out.println("");
+
+            }
+        }
+
+//        double mA[]=Utils.findRoot(array2,array,false);
+//        System.out.println(mA);
     }
-
-
-    //ввод двумерного массива
-//    public static void arrayDouble(String[][] arg) throws IOException {
-//        System.out.println("Введите размер матрицы:");
-//        //строка ввода из консоли;
-//        String n = getOneLine();
-//        //устанавливаем размер матрицы;
-//        int n = Integer.parseInt(String n);
-//        double arr[][] = new double[n][n];
-//        System.out.print("Введите двумерный массив:\n{");
-//        for (int i = 0; i < arr.length; i++)
-//            for (int j = 0; j < arr.length; j++) {
-//                System.out.println("Введите [" + i + "][" + j + "]-ый компонент массива");
-//            }
-//    }
 }

9644f2b602ae33dcb8f2ff4819a8b76a4b953d73
freeRain19 <tumilovi4@gmail.com>
2016-05-06 12:02:18 +0300

    Pushkin

---
+++ src/by/it/sergeev/jd01_06/Main.java
@@ -0,0 +1,24 @@
+package by.it.sergeev.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        StringBuilder pushkin=new StringBuilder(Pushkin.getString());
+        Pattern p1 = Pattern.compile("[а-яА-ЯёЁ]{5,}");
+        Matcher m1 = p1.matcher(pushkin);
+        while (m1.find())
+        {
+            int start=m1.start();
+            int length=m1.group().length();
+            pushkin.setCharAt(start+4,'#');
+               if (length>6)
+               {
+               pushkin.setCharAt(start+6,'#');
+               }
+        }
+        System.out.println(pushkin);
+    }
+}

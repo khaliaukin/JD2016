9d7112a18a6d43eda24181c465f804f9446514c4
Dmitry Sergeev <specmedia@icloud.com>
2016-05-08 00:15:08 +0300

    B1 доработал

--- src/by/it/sergeev/jd01_06/TaskB1.java
+++ src/by/it/sergeev/jd01_06/TaskB1.java
@@ -1,20 +1,24 @@
 package by.it.sergeev.jd01_06;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB1 {
     public static void PushkinB() {
+        char[] chars = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩ".toCharArray();
         StringBuffer sb = new StringBuffer();
         Matcher m = Pattern.compile("(?i)\\b([а-яА-ЯёЁ])(.+?)\\b").matcher(Pushkin.getString());
         int count = 0;
         while (m.find()) {
-            if (m.group().length() == 5)
+            if (m.group(2).length() == 4 && Arrays.binarySearch(chars, m.group(1).toUpperCase().charAt(0)) >=0 )
             {
                 count++;
-                m.appendReplacement(sb, "_");
+                System.out.print(m.group() + " ");
+                m.appendReplacement(sb, "___");
             }
         }
+        System.out.println();
         System.out.println("Количество слов: " + count + '\n');
         m.appendTail(sb);
         System.out.println(sb);

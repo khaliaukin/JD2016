a0ef1045b20b31c3e84e4f185eb76be8192b8d6c
Mikki Mauz <2756903@gmail.com>
2016-05-10 12:29:44 +0200

    jd01_06 TaskA (1+, 2+, 3+) jd01_06 TaskB (1-, 2+, 3+-)

--- src/by/it/luksha/jd01_06/Runner.java
+++ src/by/it/luksha/jd01_06/Runner.java
@@ -2,13 +2,13 @@ package by.it.luksha.jd01_06;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println(Data.lykomorie);
 
-        //TaskA.task1();
-        //TaskA.task2();
-        //TaskA.task3();
+        TaskA.task1();
+        TaskA.task2();
+        TaskA.task3();
 
         //TaskB.task1();
         TaskB.task2();
+        TaskB.task3("о");
     }
 }
--- src/by/it/luksha/jd01_06/TaskB.java
+++ src/by/it/luksha/jd01_06/TaskB.java
@@ -1,9 +1,13 @@
 package by.it.luksha.jd01_06;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB {
+    /**
+     * Метод удаляет из текста все слова длины 5, начинающиеся на согласную букву.
+     */
     public static void task1() {
         System.out.println("1. Из текста удалить все слова длины 5, начинающиеся на согласную букву.");
 
@@ -22,6 +26,9 @@ public class TaskB {
         System.out.println(text);
     }
 
+    /**
+     * Метод выводит все предложения заданного текста в порядке возрастания количества слов в каждом из них. В предложениях убирает переносы строк.
+     */
     public static void task2() {
         System.out.println("2. Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них. В предложениях убрать переносы строк.");
 
@@ -37,7 +44,6 @@ public class TaskB {
             text.replace(start, start+1, " ");
 
         }
-        System.out.println(text);
 
         //разбиение строки на массив предложений и вывод в консоль
         String[] arrayOfText = text.toString().split("([\\.]{1}\\s)|(! )|\\?\\s");
@@ -71,5 +77,58 @@ public class TaskB {
             System.out.println(k + ". " + element);
             k++;
         }
+    }
+
+    /**
+     * Метод сортирует слова в тексте по убыванию количества вхождений заданного символа, а в случае равенства — по алфавиту.
+     * @param sub заданный символ
+     */
+    public static void task3(String sub) {
+
+        System.out.println("3. Отсортировать слова в тексте по убыванию количества вхождений заданного символа, а в случае равенства — по алфавиту.");
+
+        //разбивание строки с текстом на массив строк со словами
+        String[] words = Data.lykomorie.split("( )|(\\s-\\s+)|(\\p{Punct}+\\s)|(\\n)");
+
+        //переводим все символы слов в нижний регистр
+        System.out.println("До сортировки:");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+            System.out.println((i+1) + ". " + (words[i]));
+        }
+
+        //сортировка по алфавиту
+        Arrays.sort(words);
+
+        //убывающая сортировка по количеству вхождений заданного символа
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length - 1; j++) {
+                int k = counterOccurrence(words[j], sub);
+                int l = counterOccurrence(words[j+1], sub);
+                if (k < l)
+                {
+                    String tmp = words[j];
+                    words[j] = words[j+1];
+                    words[j+1] = tmp;
+                }
+            }
+        }
+
+        System.out.println("После сортировки по количеству вхождений символа " + sub + " :");
+        int count = 1;
+        for (String word: words) {
+            System.out.println(count + ". " + (word));
+            count++;
+        }
+    }
+
+    /**
+     * Метод ищет кол-во вхождений подстроки в строку
+     * @param string строка для поиска
+     * @param sub подстрока, которую ищем
+     * @return количество вхождений
+     */
+    public static int counterOccurrence(String string, String sub) {
+        return (string + "\0").split(sub).length - 1;
     }
 }

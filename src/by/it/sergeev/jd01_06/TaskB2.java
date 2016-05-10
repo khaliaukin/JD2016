e06020ce6e9b0bed57e4faa34d396c6eb0c0154d
Dmitry Sergeev <specmedia@icloud.com>
2016-05-10 12:36:04 +0300

    B2

--- src/by/it/sergeev/jd01_06/TaskB2.java
+++ src/by/it/sergeev/jd01_06/TaskB2.java
@@ -1,27 +1,31 @@
 package by.it.sergeev.jd01_06;
 
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
 public class TaskB2 {
+
     public static void PushkinB22() {
-        String str = Pushkin.getString();
-        int wordsCount;
-        String[] sentences = str.split("\\.|\\!|\\?");
-        // Делим на предложения. Подразумеваем, что делятся через точку, восклицательный знак или знак вопроса.
+        StringBuilder text = new StringBuilder(Pushkin.getString());
+        String s[] = text.toString().trim().split("[.!?]+\\s*");//делим текст на предложения.
+        int arr[] = new int[s.length]; //создаём массив с предложениями
+        int k[] = new int[s.length]; //создаём копию массива для последующего сравнения
 
-
-        for (int i=0;i<sentences.length;i++)
-        {
-            System.out.println("Предложение "+Integer.toString(i+1)+":"+sentences[i]);
-            // делим предложение на слова. Слова через пробелы (количество пробелов не учитывается)
-            String [] words = sentences[i].split("\\s+");
-            wordsCount = 0;
-            for (int j=0;j<words.length;j++)
-                // словом будет считаться строка символов не являющаяся пробелом и длиной больше 1
-                if ((words[j]!=" ") && (words[j].length()>1)) wordsCount++;
-            System.out.println("Количество слов в предложении: "+wordsCount);
+        for (int i = 0; i <= s.length-1; i++) {
+            int t = 0;
+            String w[] = s[i].toString().trim().split(" "); //количество слов в каждом предложении.
+            for (int j = 0; j <= w.length-1; j++) {
+                t++;
+            }
+            arr[i] = t;
+            k[i] = t;
+        }
+        Arrays.sort(arr); // сортируем массив по количеству слов в предложении
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr.length - 1; j++) {
+                if (arr[i] == k[j]) {
+                    System.out.println(s[j].replaceAll("\n", " ")); //удаление переноса строки
+                }
+            }
         }
     }
 }

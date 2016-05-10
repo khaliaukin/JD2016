3739000506d36fd4b6bcb6181855a76ec03011a7
Mikki Mauz <2756903@gmail.com>
2016-05-10 22:08:10 +0200

    jd01_05 Task (3+)

--- src/by/it/luksha/jd01_05/Runner.java
+++ src/by/it/luksha/jd01_05/Runner.java
@@ -4,9 +4,14 @@ import by.it.luksha.jd01_05.Math_var7.*;
 
 public class Runner {
     public static void main(String[] args) {
+        //1
         System.out.printf("Результат для разбивки на части %4.5f \n", Task1.theFirst(12.75));
         System.out.printf("Результат для целой записи %4.5f \n", Task1.theSecond(12.75));
 
+        //2
         System.out.printf("Решение функции %4.5f \n", Task2.calculate(1.21, 0.371));
+
+        //3
+        Task3.createTable(3.67, -10, 1, 1.25);
     }
 }

62ff36b409e132d608b4ceaa7fd9023f2e23c8cd
tanyakust@gmail.com <sunrise2016>
2016-05-15 09:53:52 +0300

    JD01_06: TaskA

---
+++ src/by/it/kust/jd01_06/Runner.java
@@ -0,0 +1,15 @@
+package by.it.kust.jd01_06;
+
+/**
+ * Created by User on 14.05.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("TaskA1");
+        TaskA.replaceSymbol();
+        System.out.println("\nTaskA2");
+        TaskA.numberOfWords();
+        System.out.println("\nTaskA3");
+        TaskA.vowel();
+    }
+}

4a7425acff376ad32db7334389c02c63dec7a885
Igor Sereda <sis.public@gmail.com>
2016-05-15 13:55:26 +0300

    Задание jd01_06: небольшие исправления

--- src/by/it/sereda/jd01_06/B1.java
+++ src/by/it/sereda/jd01_06/B1.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class B1 {
 
     static boolean soGlasnaya(String word){
-        //паттерн для гласных букв
+        //паттерн для согласных букв
         String soglas="бвгджзклмнпрстфхцчшщБВГДЖЗКЛМНПРСТФХЦЧШЩ";
         char first=word.charAt(0);
         //вернем true если согласная в начале
@@ -14,14 +14,9 @@ public class B1 {
     }
 
     public static void removeWordsWithLength() {
-        //Получим строку в которой можно проводить замену символов.
-        //это StringBuilder. В String такое делать нельзя.
+
         StringBuilder text=new StringBuilder(Data.lukomor);
-        //сначала переберем все слова.
-        //Нас интересуют слова с длиной 5
-        //Паттерн для таких слов [а-яА-ЯёЁ]{5}
         Pattern p=Pattern.compile("[а-яА-ЯёЁ]+");
-        //Теперь получим matcher - эта структура умеет находить слова по паттерну
         Matcher m=p.matcher(text);
         while (m.find()) {
             if ((soGlasnaya(m.group()) && m.group().length()==5)) {
@@ -29,7 +24,6 @@ public class B1 {
                 m.reset();
                 }
             }
-        //конец. печатаем массив
         System.out.println(text);
     }
 }

29a2a3427ca61f07ac33cc30ee78241783a09b2c
Noviko <helloiamkseniya.novik@gmail.com>
2016-05-16 10:44:28 +0300

    reload 16.05 - 1

---
+++ src/by/it/khalyavkin/jd01_05/Fun.java
@@ -0,0 +1,13 @@
+package by.it.khalyavkin.jd01_05;
+
+import static java.lang.Math.*;
+/**
+ * Created by user_2 on 06.05.2016.
+ */
+public class Fun {
+    public static double f1(double a, double b ) {
+        double y=Math.tan((a+b)*(a+b));
+        return y;
+
+    }
+}

f47cb983216d630623758ed6e678b6a108d92c45
Sinkevich <SleePokeR@gmail.com>
2016-05-16 12:05:18 +0300

    Задание jd01_09 наброски.

---
+++ src/by/it/sinkevich/jd01_09/operations/IAriphmeticOperation.java
@@ -0,0 +1,9 @@
+package by.it.sinkevich.jd01_09.operations;
+
+/**
+ * Created by Computer on 16.05.2016.
+ *
+ * @author Sinkevich Denis
+ */
+public interface IAriphmeticOperation extends IAddition, ISubstraction, IMultiplication, IDivision {
+}
